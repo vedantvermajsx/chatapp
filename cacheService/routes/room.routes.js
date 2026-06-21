dbd0f70901ec8910ec0703dbfbf7909d7e8fa8e7
vedantverma.jsx <vedantverma303@gmail.com>
2026-06-21 12:50:02 +0530

    renamed

--- cacheService/routes/room.routes.js
+++ cacheService/routes/room.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import roomCacheService from '../services/RoomCacheService';
+import roomCacheService from '../services/RoomCacheService.js';
 
 const router = express.Router();
 
